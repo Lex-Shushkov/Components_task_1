7fe434a97b28c0e3d8e1f0e273899ca8d1b86a1e
Aleksey <shushkov_a@mail.ru>
2024-10-13 17:06:26 +0600

    добавлен второй обработчик

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ export const App = () => {
 	const [value, setValue] = useState('');
 	const [list, setList] = useState([]);
 	const [error, setError] = useState('');
-	const [isValueVaild, setIsValueVaild] = useState();
+	const [isValueVaild, setIsValueVaild] = useState(false);
 
 	const onInputButtonClick = () => {
 		let promptValue = prompt('Введите значение');
@@ -16,6 +16,17 @@ export const App = () => {
 			setValue(promptValue);
 			setError('');
 			setIsValueVaild(true);
+		}
+	};
+
+	const onAddButtonClick = () => {
+		const id = Date.now();
+		// const updatedList = [...list, { id, value }];
+		if (isValueVaild) {
+			setValue('');
+			setError('');
+			setIsValueVaild(false);
+			setList((currentList) => [...currentList, { id, value }]);
 		}
 	};
 
@@ -31,7 +42,11 @@ export const App = () => {
 				<button className={styles.button} onClick={onInputButtonClick}>
 					Ввести новое
 				</button>
-				<button className={styles.button} disabled={!isValueVaild}>
+				<button
+					className={styles.button}
+					onClick={onAddButtonClick}
+					disabled={!isValueVaild}
+				>
 					Добавить в список
 				</button>
 			</div>
@@ -39,7 +54,13 @@ export const App = () => {
 				<h2 className={styles['list-heading']}>Список:</h2>
 				<p className={styles['no-margin-text']}>Нет добавленных элементов</p>
 				<ul className={styles.list}>
-					<li className={styles['list-item']}>Первый элемент</li>
+					{list.map((item) => (
+						<li key={item.id} className={styles['list-item']}>
+							{item.value}
+						</li>
+					))}
+
+					{/* <li className={styles['list-item']}>Первый элемент</li> */}
 				</ul>
 			</div>
 		</div>
